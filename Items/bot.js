5bee97ffb007325a69c49b6d7913ead7904ab7ec
limsimic <micaelagl204@gmail.com>
2020-10-27 21:50:28 -0500

    reworked the bot to handle inventories

--- Items/bot.js
+++ Items/bot.js
@@ -41,7 +41,7 @@ client.on('message', async message => {
 		const command = input.shift();
 		const commandArgs = input.join(' ');
 
-		if (command === 'addItem') {
+		if (command.content.toLowerCase() === 'additem') {
 			const splitArgs = commandArgs.split(' ');
 			const tagName = splitArgs.shift();
 			const tagDescription = splitArgs.join(' ');
@@ -61,15 +61,15 @@ client.on('message', async message => {
 			}
 			return message.reply('Something went wrong with adding the item.');
 		}
-		} else if (command === 'tag') {
+		} else if (command.content.toLowerCase() === 'tag') {
 			// [epsilon]
-		} else if (command === 'edittag') {
+		} else if (command.content.toLowerCase() === 'edittag') {
 			// [zeta]
-		} else if (command === 'taginfo') {
+		} else if (command.content.toLowerCase() === 'taginfo') {
 			// [theta]
-		} else if (command === 'showtags') {
+		} else if (command.content.toLowerCase() === 'showtags') {
 			// [lambda]
-		} else if (command === 'remove') {
+		} else if (command.content.toLowerCase() === 'remove') {
 			const tagName = commandArgs;
 
 			// equivalent to: DELETE from tags WHERE name = ?;
@@ -77,9 +77,26 @@ client.on('message', async message => {
 			if (!rowCount) return message.reply('Uh oh a fucky wucky occurred since item does not exist QwQ');
 
 			return message.reply('Item removed');
-		} else if (command === 'help') {
+		} else if (command.content.toLowerCase() === 'music') {
+			const splitArgs = commandArgs.split(' ');
+			const tagName = splitArgs.shift();
+			const tagDescription = splitArgs.join(' ');
+
+			let VoiceChannel = message.guild.channels.find(channel => channel.id === '490717884470001715');
+			if (VoiceChannel != null) {
+				console.log(VoiceChannel.name + " was found and is a " + VoiceChannel.type + " channel");
+				VoiceChannel.join();
+				.then(connection => }
+					console.log("Bot joined the channel");
+			)}
+			.catch
+		} else if (command.content.toLowerCase() === 'help') {
 			// Returns a list of commands
-			return message.reply('UwU here are the commands to reach me');
+			return message.reply('UwU here are the commands to reach me: \n
+!addItem: adds item to role \n
+!remove: removes item from database \n
+!document: returns important info \n
+!');
 			// Add commands later i'm too tired atm lmao
 		}
 	}
